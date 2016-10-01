a23f9ca74361c750c04f5889bd9bcda282136fbd
Alex <alex.user03@gmail.com>
2016-10-01 19:18:13 -0400

    Added neuron logic
    Activate function and feed forward logic

---
+++ Neuron.java
@@ -0,0 +1,47 @@
+package ANN;
+
+public class Neuron {
+
+	private double[] synapses;
+	private double bias;
+	public static final String[] FUNCTIONS = {"sigmoid","hyperbolic tangent","basic treeshold","linear"};
+	public static String function = FUNCTIONS[0];
+	
+	public Neuron(int numOfSynapses){
+		synapses = new double[numOfSynapses];
+		initsynapses();
+	}
+	
+	private void initsynapses(){
+		for(int i=0; i< synapses.length; i++){
+			synapses[i] = Math.random();
+		}
+		bias = Math.random();
+		
+	}
+	
+	public double feedSignals(double[] inputs){
+		double signal = 0;
+		for(int i=0; i<inputs.length; i++){
+			signal += inputs[i]*synapses[i];
+		}
+		signal += bias;
+		
+		return activate(signal);
+	}
+	
+	private double activate(double x){
+		switch(Neuron.function){
+			case "sigmoid":
+				return 1/(1+ Math.pow(Math.E, -x ));
+			case "hyperbolic tangent":
+				return (1- Math.pow(Math.E, -x))/(1+Math.pow(Math.E, -x) );
+			case "basic treeshold":
+				return (x < 0.5) ? 0 : 1;
+			case "linear":
+				return x;
+			default: //error
+				return 0;
+		}
+	}
+}
